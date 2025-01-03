12f1c21aa822ebf4240f681abfd3e41164788677
sam112358 <samarth.khandelwal39@gmail.com>
2025-01-04 03:31:40 +0530

    contains duplicate

---
+++ src/main/java/com/samarth/revision/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package main.java.com.samarth.revision;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for (int num : nums) {
+            if(!hs.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
