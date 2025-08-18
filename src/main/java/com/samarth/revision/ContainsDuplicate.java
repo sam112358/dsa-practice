4436f44289e5e0cd75b3b6b1427a6d7c523b13d1
sam112358 <samarth.khandelwal39@gmail.com>
2025-08-19 01:49:29 +0530

    contains duplicate

--- src/main/java/com/samarth/revision/ContainsDuplicate.java
+++ src/main/java/com/samarth/revision/ContainsDuplicate.java
@@ -4,18 +4,19 @@ import java.util.HashMap;
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
-        int[] nums = {1};
+        int[] nums = {1,2,3,1};
         System.out.println(containsDuplicate(nums));
     }
 
     public static boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i : nums) {
-            if(map.containsKey(i)) {
+        if (nums.length <= 1)
+            return false;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i : nums) {
+            if (hm.containsKey(i))
                 return true;
-            } else {
-                map.put(i, 1);
-            }
+            else
+                hm.put(i, 1);
         }
         return false;
     }
